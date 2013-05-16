1678b60d84358f6347af8bba931753673c3e5d10
Chris Noden <chris@noden.net>
2013-05-16 12:51:15 +0100

    Setting up Test suite
    Also moved Talkback to the new Github package

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -6,13 +6,9 @@
  * @copyright (c) 2009 to 2013 Chris Noden
  */
 
-
-require_once('PHPUnit/Framework/TestCase.php');
-if (file_exists(dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR .'autoload.php')) {
-    require_once(dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR .'autoload.php');
-} else {
-    die("Not part of composer install");
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Run "composer install --dev" to create autoloader.');
 }
 
-
-
+$loader = require $autoloadFile;
+$loader->add('Synergy\Tests', __DIR__);
