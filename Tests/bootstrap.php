ac477a1a3329adea115bb32f862ccf27b47908ef
Chris Noden <chris@noden.net>
2013-05-23 15:36:25 +0100

    Improved Unit Tests bootstrap
    bootstrap.php updated to force bundle autoloader if run from within the
    synergy-bundle

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -4,16 +4,35 @@
  *
  * PHP version 5
  *
- * @category  Project:Synergy
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @category  File
  * @package   Synergy
  * @author    Chris Noden, @chrisnoden
- * @copyright (c) 2009 to 2013 Chris Noden
- * @link      http://chrisnoden.com
- * @license   http://opensource.org/licenses/LGPL-3.0
+ * @copyright 2009-2013 Chris Noden
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link      https://github.com/chrisnoden
  */
-if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+
+$autoloadFile = dirname(dirname(dirname(dirname(__FILE__)))) . DIRECTORY_SEPARATOR . 'autoload.php';
+if (!is_file($autoloadFile)) {
+    $autoloadFile = __DIR__ . '/../vendor/autoload.php';
+}
+
+if (is_file($autoloadFile)) {
+    $loader = include_once $autoloadFile;
+    $loader->add('Synergy\Tests', __DIR__);
+} else {
     throw new \LogicException('Run "composer install --dev" to create autoloader.');
 }
 
-$loader = require $autoloadFile;
-$loader->add('Synergy\Tests', __DIR__);
