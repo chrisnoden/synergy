56ffd5ecce00c72e3c9f85b4440e20d13511cc59
Chris Noden <chris@noden.net>
2013-06-08 21:36:42 +0100

    New SmartyTemplate class
    Templates can now be Smarty format

---
+++ src/Synergy/Project/Web/Template/SmartyTemplate.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Created by Chris Noden using JetBrains PhpStorm.
+ * 
+ * PHP version 5
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @category  File
+ * @package   Synergy
+ * @author    Chris Noden <chris.noden@gmail.com>
+ * @copyright 2009-2013 Chris Noden
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link      https://github.com/chrisnoden
+ */
+
+namespace Synergy\Project\Web\Template;
+
+use Synergy\Exception\SynergyException;
+
+/**
+ * Class SmartyTemplate
+ *
+ * @category Synergy\Project\Web\Template
+ * @package  Synergy
+ * @author   Chris Noden <chris.noden@gmail.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link     https://github.com/chrisnoden/synergy
+ */
+class SmartyTemplate extends TemplateAbstract
+{
+
+    /**
+     * @var \Smarty
+     */
+    private $_loader;
+
+
+    /**
+     * Initialise Twig
+     *
+     * @return void
+     */
+    protected function initTemplateEngine()
+    {
+        $this->_loader = new \Smarty();
+        $this->_loader->muteExpectedErrors();
+        $this->_loader->setTemplateDir($this->templateDir);
+        $this->_initSmartyCache();
+    }
+
+
+    /**
+     * template render output
+     *
+     * @return string template render output
+     * @throws SynergyException
+     */
+    protected function getRender()
+    {
+        if (isset($this->templateFile)) {
+            $this->assignSmartyVariables();
+            $render = $this->_loader->fetch($this->templateFile);
+            return $render;
+        } else {
+            throw new SynergyException(
+                sprintf(
+                    'Invalid call to %s without setting templateFile',
+                    __METHOD__
+                )
+            );
+        }
+    }
+
+
+    /**
+     * Set the variables so Smarty can access them
+     *
+     * @return void
+     */
+    protected function assignSmartyVariables()
+    {
+        $this->_loader->assign($this->parameters);
+    }
+
+
+    /**
+     * Location of the template cache directory
+     *
+     * @param string $dir absolute location of the template cache directory
+     *
+     * @return void
+     */
+    public function setCacheDir($dir)
+    {
+        $dir .= DIRECTORY_SEPARATOR . 'smarty';
+        parent::setCacheDir($dir);
+    }
+
+
+    /**
+     * Prepares the cache folder for Smarty
+     *
+     * @return void
+     */
+    private function _initSmartyCache()
+    {
+        if (!is_dir($this->cacheDir)) {
+            $this->mkdir($this->cacheDir, true);
+            // compiled templates dir
+            $path
+                = $this->cacheDir .
+                DIRECTORY_SEPARATOR .
+                'templates_c' .
+                DIRECTORY_SEPARATOR;
+            if ($this->mkdir($path, false)) {
+                $this->_loader->setCompileDir($path);
+            }
+
+            // cache dir
+            $path
+                = $this->cacheDir .
+                DIRECTORY_SEPARATOR .
+                'cache' .
+                DIRECTORY_SEPARATOR;
+            if ($this->mkdir($path, false)) {
+                $this->_loader->setCacheDir($path);
+            }
+
+            // configs dir
+            $path
+                = $this->cacheDir .
+                DIRECTORY_SEPARATOR .
+                'configs' .
+                DIRECTORY_SEPARATOR;
+            if ($this->mkdir($path, false)) {
+                $this->_loader->setConfigDir($path);
+            }
+        }
+    }
+
+}
